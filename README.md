e211c24a7c8a38082220a9a051dbe4bcb1302988
ajprobel <ajprobel@gmail.com>
2024-03-11 19:51:40 -0700

    update README, add image assets, update JS

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# work-day-planner
-Simple work day planner made for the Module 5 Challenge
+# Simple Work Day Scheduler
+
+## Description
+
+A simple planner/scheduler for a work day can be surprisingly helpful to add structure and make our hectic lives easier. Instead of having a notepad and paper to write down tasks throughout the day, what better way to have a convenient planner exists other than having one right in your browser, as a web page? 
+
+This Work Day Scheduler is a dynamic web page that allows users to log their tasks each day and highlights the current hour's tasks and reminders. With this dynamically changing style as the hours pass during the day, users will be able to see tasks/reminders in the past, current tasks to do that hour, and see what lies ahead in the future hours. Hopefully this can aid people to be more productive and add structure to their busy work day!
+
+To complete this project, I utilized Day.js functionality to allow the website to track the current time of the user. Using custom dataset attributes, the styling of the time blocks responds to the current Day.js information. Additionally, I utilized JQuery and Local Storage to add functionality for users to store tasks with the click of a button, and have those tasks be preserved upon refreshing or reloading the page. This was a great way to keep my JavaScript and API skills sharp!
+
+## Installation
+
+N/A
+
+## Usage
+
+To use the scheduler, use this [link](https://ajprobel.github.io/work-day-planner/)
+
+You will be greated by a simple header that says "Work Day Scheduler." Inside the header, below that, you'll find the current date and time (this site utilizes Day.js to gather current time information).
+
+Below the header are various time blocks separated by hour - these range from 9AM to 5PM: a standard work day. Each row contains a label for the time, a text description box, and a "save" button. Each text description box will be a different color depending on the time: time blocks in the past will be **grey**; time blocks in the future will be **green**; and the current time block is **red.**
+
+Click each colored box and type an event/reminder that occurs in that hour. Once done typing, click on the blue "save" button on the right-most side of the row. That event will now be saved and preserved upon page refresh/reload.
+
+**The first screenshot shows how the webpage will look outside of working hours - all grey boxes**
+
+![screenshot1](./assets/images/screenshot1.jpg)
+![screenshot2](./assets/images/screenshot2.png)
+![screenshot3](./assets/images/screenshot3.png)
+
+## Credits
+
+Starter code provided by the Programming Bootcamp of UNC Chapel Hill: HTML and CSS starter Framework
+Website functionality created by me, James Probel
+
+Thank you to the JQuery API documentation for guidelines on usage:
+https://api.jquery.com/children/
+https://api.jquery.com/has-attribute-selector/
+
+
+
+## License
+
+Refer to the included project MIT license.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,89 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
 
-});
-
-// Time Functionality
-var date = document.querySelector("#currentDay");
-var time = document.querySelector("#currentTime");
-function refreshTime() {
-  var timerInterval = setInterval(function () {
-    var today = dayjs();
-    $('#currentDay').text(today.format('MMM D, YYYY'));
-    $('#currentTime').text(today.format('h:mm:ss a'));
-    if ((Number(currentHour) >= 0 && Number(currentHour) <= 6) || (Number(currentHour) >= 19)) {
-      $(".spacer").text(" ☾ ");
+  // Checks if there's previous local storage data; if true, puts that data in the description boxes
+  function renderPreviousToDos() {
+    if (localStorage.getItem("9")) {
+      var previousToDo = localStorage.getItem("9");
+      $("#hour-9").children(".description").text(previousToDo);
     }
-  }, 1000);
-}
-refreshTime();
+    if (localStorage.getItem("10")) {
+      var previousToDo = localStorage.getItem("10");
+      $("#hour-10").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("11")) {
+      var previousToDo = localStorage.getItem("11");
+      $("#hour-11").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("12")) {
+      var previousToDo = localStorage.getItem("12");
+      $("#hour-12").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("13")) {
+      var previousToDo = localStorage.getItem("13");
+      $("#hour-1").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("14")) {
+      var previousToDo = localStorage.getItem("14");
+      $("#hour-2").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("15")) {
+      var previousToDo = localStorage.getItem("15");
+      $("#hour-3").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("16")) {
+      var previousToDo = localStorage.getItem("16");
+      $("#hour-4").children(".description").text(previousToDo);
+    }
+    if (localStorage.getItem("17")) {
+      var previousToDo = localStorage.getItem("17");
+      $("#hour-5").children(".description").text(previousToDo);
+    }
+  }
+  renderPreviousToDos();
 
-// Compares the hour of each time block to the current hour, assigns new classes as necessary
-var currentHour = dayjs().format('H');
-for (var n = 0; n < 9; n++) {
-  var dataHour = document.body.children[1].children[n].getAttribute("data-hour");
+  // Time Functionality
+  function refreshTime() {
+    var timerInterval = setInterval(function () {
+      var today = dayjs();
+      $('#currentDay').text(today.format('MMM D, YYYY'));
+      $('#currentTime').text(today.format('h:mm:ss a'));
+      if ((Number(currentHour) >= 0 && Number(currentHour) <= 6) || (Number(currentHour) >= 19)) {
+        $(".spacer").text(" ☾ ");
+      }
+    }, 1000);
+  }
+  refreshTime();
 
-  if (Number(dataHour) < Number(currentHour)) {
-    document.body.children[1].children[n].setAttribute("class", "row time-block past");
-  } else if (Number(dataHour) == Number(currentHour)) {
-    document.body.children[1].children[n].setAttribute("class", "row time-block present");
-  } else if (Number(dataHour) > Number(currentHour)) {
-    document.body.children[1].children[n].setAttribute("class", "row time-block future");
+  // Compares the hour of each time block to the current hour, assigns new classes as necessary
+  var currentHour = dayjs().format('H');
+  for (var n = 0; n < 9; n++) {
+    var dataHour = document.body.children[1].children[n].getAttribute("data-hour");
+
+    if (Number(dataHour) < Number(currentHour)) {
+      document.body.children[1].children[n].setAttribute("class", "row time-block past");
+    } else if (Number(dataHour) == Number(currentHour)) {
+      document.body.children[1].children[n].setAttribute("class", "row time-block present");
+    } else if (Number(dataHour) > Number(currentHour)) {
+      document.body.children[1].children[n].setAttribute("class", "row time-block future");
+    }
+
   }
 
-}
+  // adds an event listener on the time block, and does the following:
+  // checks to see if the user hit the "save" button
+  // if true, selects the 'this' hour and textarea to be saved in local storage
+  var timeBlock = $(".time-block");
+  timeBlock.on("click", function (event) {
+    var element = event.target;
 
+    if (element.matches("button") || element.matches("i")) {
+      var toDo = this.children[1].value;
+      var savedHour = this.getAttribute("data-hour");
+      console.log(toDo);
+      console.log(this.getAttribute("data-hour"));
+      localStorage.setItem(savedHour, toDo);
+    }
+  })
 
-// Checks if there's previous local storage data; if true, puts that data in the description boxes
-function renderPreviousToDos() {
-  if (localStorage.getItem("9")) {
-    var previousToDo = localStorage.getItem("9");
-    $("#hour-9").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("10")) {
-    var previousToDo = localStorage.getItem("10");
-    $("#hour-10").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("11")) {
-    var previousToDo = localStorage.getItem("11");
-    $("#hour-11").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("12")) {
-    var previousToDo = localStorage.getItem("12");
-    $("#hour-12").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("13")) {
-    var previousToDo = localStorage.getItem("13");
-    $("#hour-1").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("14")) {
-    var previousToDo = localStorage.getItem("14");
-    $("#hour-2").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("15")) {
-    var previousToDo = localStorage.getItem("15");
-    $("#hour-3").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("16")) {
-    var previousToDo = localStorage.getItem("16");
-    $("#hour-4").children(".description").text(previousToDo);
-  }
-  if (localStorage.getItem("17")) {
-    var previousToDo = localStorage.getItem("17");
-    $("#hour-5").children(".description").text(previousToDo);
-  }
-}
-renderPreviousToDos();
-
-
-// adds an event listener on the time block, and does the following:
-// checks to see if the user hit the "save" button
-// if true, selects the 'this' hour and textarea to be saved in local storage
-var timeBlock = $(".time-block");
-timeBlock.on("click", function (event) {
-  var element = event.target;
-
-  if (element.matches("button") || element.matches("i")) {
-    var toDo = this.children[1].value;
-    var savedHour = this.getAttribute("data-hour");
-    console.log(toDo);
-    console.log(this.getAttribute("data-hour"));
-    localStorage.setItem(savedHour, toDo);
-  }
-})
+});
